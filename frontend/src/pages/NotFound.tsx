a6bc622093bd9e98d903195a263cc87436647036
Casper Sigfrids Fintling <casper.sigfridsfint@learnet.se>
2024-09-20 13:56:19 +0200

    fixed styling even more on 404

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,9 +1,10 @@
 const NotFound = () => {
   return (
-    <div>
-      <h1 className="text-center text-8xl font-bold my-auto">404</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-center text-8xl font-bold">404</h1>
       <p className="text-center font-bold">Page not found</p>
     </div>
   )
 }
+
 export default NotFound
